3df518cde8bd2b780fccd6196b8e7b714630110c
sandeepvarma1010 <103293241+sandeepvarma1010@users.noreply.github.com>
2024-08-14 16:53:02 -0700

    Second final  commit

--- src/components/transactions/TransactionPage.js
+++ src/components/transactions/TransactionPage.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import './TransactionPage.css';
+import { useNavigate } from 'react-router-dom';
+import './TransactionPage.css'; // Ensure this is imported
 
 const TransactionPage = ({ onLogout }) => {
-    const navigate = useNavigate(); 
-    const location = useLocation(); 
+    const navigate = useNavigate(); // Define the navigate function
 
     return (
-        <div className="transaction-page">
-            <h2 className="dashboard-header">Welcome to Your Dashboard</h2>
-            {location.state && location.state.message && (
-                <div className="success-message">{location.state.message}</div>
-            )}
+        <div>
+            <div className="transaction-page-header">
+                <h2>Welcome to Your Dashboard</h2>
+                <button onClick={onLogout} className="logout-button">
+                    Logout
+                </button>
+            </div>
             <div className="button-container">
                 <button onClick={() => navigate('/add-transaction')} className="dashboard-button">
                     Add Transaction
                 </button>
                 <button onClick={() => navigate('/transaction-list')} className="dashboard-button">
                     Transaction List
-                </button>
-                <button onClick={onLogout} className="dashboard-button">
-                    Logout
                 </button>
             </div>
         </div>
